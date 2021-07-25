8b33564869300ec63a0666aa618a27c78b35097a
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2021-07-25 23:10:31 +0700

    add print

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("file"))))
+	fmt.Println("starting donkey")
 	if err := http.ListenAndServe(":8070", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
+
 	}
 }

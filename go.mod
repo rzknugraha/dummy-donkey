90757fd6a25a71787941c42725b822e2a3fd92f7
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2021-07-26 00:34:00 +0700

    change to mux

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rzknugraha/dummy-donkey
 
 go 1.13
 
-require github.com/rs/cors v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.8.0
+)
--- main.go
+++ main.go
@@ -1,29 +1,57 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
+// func main() {
+// 	fs := http.FileServer(http.Dir("file/zorro-file/"))
+// 	http.Handle("/file/", http.StripPrefix("/file/", fs))
+
+// 	http.HandleFunc("/", serve)
+
+// 	fmt.Println("starting donkey")
+// 	if err := http.ListenAndServe(":8070", nil); err != nil {
+// 		log.Fatal("ListenAndServe: ", err)
+
+// 	}
+// }
+
 func main() {
-	fs := http.FileServer(http.Dir("file/zorro-file/"))
-	http.Handle("/file/", http.StripPrefix("/file/", fs))
-	http.HandleFunc("/", serve)
+	router := NewRouter()
 
-	fmt.Println("starting donkey")
-	if err := http.ListenAndServe(":8070", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	handler := cors.Default().Handler(router)
 
+	c := cors.New(cors.Options{
+		AllowCredentials: true,
+		// Enable Debugging for testing, consider disabling in production
+		Debug:          true,
+		AllowedHeaders: []string{"*"},
+	})
+	handler = c.Handler(handler)
+
+	fmt.Printf("donkey start")
+	if err := http.ListenAndServe(":8070", handler); err != nil {
+		log.Fatal("ListenAndServe Error: ", err)
 	}
 }
 
-func serve(w http.ResponseWriter, r *http.Request) {
-	//Allow CORS here By * or specific origin
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+//NewRouter si a
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
 
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	// return "OKOK"
-	json.NewEncoder(w).Encode("OKOK")
+	// Choose the folder to serve
+	staticDir := "/file/"
+
+	// Create the route
+	router.
+		PathPrefix("/").
+		Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir("file/zorro-file/"))))
+
+	return router
 }

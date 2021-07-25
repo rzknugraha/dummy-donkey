95fdf5178a6646ea751263b7f7269e4c8739928d
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2021-07-26 00:04:53 +0700

    cors

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rzknugraha/dummy-donkey
 
 go 1.13
+
+require github.com/rs/cors v1.8.0
--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/", http.StripPrefix("/file", http.FileServer(http.Dir("file/zorro-file/"))))
+	fs := http.FileServer(http.Dir("file/zorro-file/"))
+	http.Handle("/file/", http.StripPrefix("/file/", fs))
+	http.HandleFunc("/", serve)
+
 	fmt.Println("starting donkey")
 	if err := http.ListenAndServe(":8070", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 
 	}
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	//Allow CORS here By * or specific origin
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	// return "OKOK"
+	json.NewEncoder(w).Encode("OKOK")
 }

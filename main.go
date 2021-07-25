18dded1d6e439fe74dc77f2dd3273b5a4211b4a6
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2021-07-25 23:28:48 +0700

    add zorro

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("file/zorro-file"))))
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("file/zorro-file/"))))
 	fmt.Println("starting donkey")
 	if err := http.ListenAndServe(":8070", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
